99abe7fdba07641450cdc84e16f931cb82ef6e8f
gabikohen <gabokohen@gmail.com>
2022-04-01 22:35:56 -0300

    sqlite

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-""" SECRET_KEY =  """
+""" SECRET_KEY = """
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,15 +74,12 @@ WSGI_APPLICATION = 'proyecto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'DATABASE_ENGINE':'django.db.backends.mysql',
-        'DATABASE_NAME': 'book_db',
-        'DATABASE_USER':'root',
-        'DATABASE_PASSWORD':'',
-        'DATABASE_HOST':'localhost',
-        'DATABASE_PORT':'3006',
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
 }
 
 
